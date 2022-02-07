8bb82e7c5d032702fa42d543a2f725a32af8c62b
Eniola Ajayi <ajayieniola16@gmail.com>
2022-02-07 20:59:25 +0100

    create app logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import QRCode from "qrcode";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="app"></div>;
-}
+const App = () => {
+  const [src, setSrc] = useState("");
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    QRCode.toDataURL("I like putting stuff as qr codes.").then(setSrc);
+  }, []);
+
+  const onBtnClick = () => {
+    QRCode.toDataURL(content).then(setSrc);
+  };
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  return (
+    <div className="app">
+      <h1 className="heading">QRCode Generator</h1>
+      <label for="content" className="label">
+        Enter content for qr code:
+      </label>
+      <textarea
+        id="content"
+        name="content"
+        className="textarea"
+        rows="7"
+        cols="55"
+        onChange={handleChange}
+      >
+        {content}
+      </textarea>
+      <button className="button" type="submit" onClick={onBtnClick}>
+        Generate
+      </button>
+      <figure className="image-container">
+        <img src={src} alt="a Qr code" />
+        <figcaption>Right Click and Save ✌</figcaption>
+      </figure>
+    </div>
+  );
+};
 
 export default App;
